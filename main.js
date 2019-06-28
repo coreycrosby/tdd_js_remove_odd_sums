11d41337d16a6289516cfb9164eb29c63e1adf28
Corey Crosby <ccc_13@live.ca>
2019-06-28 13:56:47 -0400

    completed exercise

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function removeOddSum() {
-}
-
+function removeOddSum(array) {
+    
+    return array.filter(num => (num.a + num.b) % 2 === 0
+    
+    );
+};
+    
 module.exports = removeOddSum;
